d027fc766031d6045565d45555e6575b281c0a46
Yash-1485 <yashparekh140805@gmail.com>
2025-06-12 00:26:19 +0530

    Added Admin and Super User Creation Process

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("",views.home,name="home"),
     path("about/",views.about,name="about"),
     path("contact/",views.contact,name="contact"),
